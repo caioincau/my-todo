2a97b2598e4a4653ab4af0db820ab4f09af5405d
Caio Incau <caioincau@gmail.com>
2015-05-01 09:36:03 -0300

    Avancando a app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require( './config/db' );
 
+var Cluster = require('cluster2');
 var express        = require( 'express' );
 var http           = require( 'http' );
 var path           = require( 'path' );
@@ -12,6 +13,10 @@ var morgan = require('morgan');
 var fs = require('fs');
 var validator = require('express-validator');
 var compression = require('compression');
+var csrf = require('csurf');
+var session = require('express-session');
+var helmet = require('helmet');
+
 var favicon = require('serve-favicon');
 
 
@@ -27,10 +32,22 @@ app.engine( 'ejs', engine );
 app.set( 'views', path.join( __dirname, 'views' ));
 app.set( 'view engine', 'ejs' );
 app.use( methodOverride());
-app.use( cookieParser());
+app.use( cookieParser("secretKey"));
 app.use( bodyParser.json());
 app.use( bodyParser.urlencoded({ extended : true }));
+
 app.use(validator());
+
+app.use(csrf({cookie:true}));
+app.use(helmet());
+
+
+app.use(function(req, res, next) {
+  res.locals._csrf = req.csrfToken();
+  next();
+});
+
+
 app.use( routes.current_user );
 app.get(  '/',            routes.index );
 app.post( '/create',      routes.create );
@@ -44,13 +61,17 @@ app.use(static(path.join( __dirname, 'public'),{maxAge: 31557600000/1000}));
 
 
 if(env=="production"){
-	var accessLogStream = fs.createWriteStream(__dirname + '/urls.log', {flags: 'a'})
+	var accessLogStream = fs.createWriteStream(__dirname + '/logs/urls.log', {flags: 'a'})
 	app.use(morgan('combined', {stream: accessLogStream}));
 	require( './config/errors' )(app);
 }else{
 	app.use(morgan());
 }
 
-http.createServer( app ).listen( app.get( 'port' ), function (){
-  console.log( 'Express rodando na porta :  ' + app.get( 'port' ));
+
+var c = new Cluster({
+    port: 3000,
+});
+c.listen(function(cb) {
+    cb(app);
 });
